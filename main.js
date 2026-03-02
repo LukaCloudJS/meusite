1201f9deaddde73e96fd72437abbac7e832e6242
LukaCloud <lukatonysh@gmail.com>
2026-03-02 18:00:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-async function savedata() {
-  const req = await fetch('http://92.118.206.166:30457/save')
-  const data = await req.json()
+function savedata() {
+  fetch('http://92.118.206.166:30457/save').then( e => {
+    e.json().then(data => {
   const test = document.getElementById('teste')
   test.innerHTML = data.join('<br>')
+    }
+    )
+  })
 }
 
 savedata()
