5b90507d51841d14028979b0766210fea1256f69
LukaCloud <lukatonysh@gmail.com>
2026-03-02 18:06:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function savedata() {
-  fetch('http://92.118.206.166:30457/save').then( e => {
+  fetch('https://92.118.206.166:30457/save').then( e => {
     e.json().then(data => {
   const test = document.getElementById('teste')
   test.innerHTML = data.join('<br>')
